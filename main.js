34356569e91319e6a159b4c3ad5c1ef2d0a71633
Vlassis Lyberopoulos <vlassisliberopoulos@gmail.com>
2025-09-08 18:55:54 +0200

    Add delete, checkbox, localStorage functionality

--- main.js
+++ main.js
@@ -1,18 +1,53 @@
 // ELEMENTS
 const add_btn = document.querySelector("#add_btn");
 const unordered_list = document.querySelector("#unordered_list");
+const completed_list = document.querySelector("#completed_list");
+const user_input = document.querySelector("#user_input");
 
 // EVENT LISTENERS
-
+// let tasksList = [];
+const tasksList = displayStoragedItems(unordered_list, completed_list);
 add_btn.addEventListener("click", add_task);
+unordered_list.addEventListener("click", delete_task);
+unordered_list.addEventListener("click", check_item);
+completed_list.addEventListener("click", delete_task);
 
 // EVENT FUNCTIONS
-
 function add_task(event) {
   event.preventDefault();
   const task_title = document.querySelector("#user_input").value.trim();
   if (!task_title) return;
-  const li = createListItem(task_title);
+  // TO-DO -> ADD TRY/CATCH | CASES : WHAT IF CREATTASK OR CREATELISTITEM THROWS?
+  const task = createTask(task_title);
+  const li = createListItem(task);
   unordered_list.append(li);
   document.querySelector("#user_input").value = "";
+  // localStorage operation
+  populateTasksList(tasksList, task);
+  populateLocalStorage("tasks", tasksList);
+}
+
+function delete_task(event) {
+  if (event.target.closest("[data-action]")) {
+    const li = event.target.closest("[data-item]");
+    li.remove();
+    // localStorage operation
+    removeTaskFromStorage(li, tasksList);
+  }
+}
+
+function check_item(event) {
+  if (event.target.closest(".checkbox")) {
+    const li = event.target.closest("[data-item]");
+    removeTaskAnimation(li);
+    li.addEventListener("animationend", () => {
+      const movedItem = removeListItem(unordered_list, li);
+      movedItem.classList.remove("delete-animation");
+      moveTo(completed_list, movedItem);
+    });
+    // localStorage operation
+    modifyTaskState(li, tasksList);
+    // TO-DOS
+    // add animation | pending
+  }
 }
--- utils.js
+++ utils.js
@@ -1,4 +1,5 @@
-function createListItem(title) {
+// ELEMENT RELATED
+function createListItem(task) {
   const li = document.createElement("li");
   const input = document.createElement("input");
   const span = document.createElement("span");
@@ -11,15 +12,20 @@ function createListItem(title) {
     "bo-rad-50",
     "li-width",
     "li-shad",
+    "list-item",
   ]);
   addClasses(span, ["sp-font", "sp-width"]);
   addClasses(delete_button, ["btn-bg", "bo-none"]);
   addClasses(icon, ["material-symbols-outlined", "ic-size-35"]);
+  delete_button.setAttribute("data-action", "delete");
+  li.setAttribute("data-item", "list_item");
+  li.setAttribute("data-id", task.id);
   input.setAttribute("type", "checkbox");
-  li.setAttribute("id", "list_item");
-  delete_button.setAttribute("id", "delete_btn");
+  input.setAttribute("class", "checkbox");
+  task.completed && input.setAttribute("checked", "");
+
   icon.textContent = "delete";
-  span.textContent = title;
+  span.textContent = task.title;
 
   delete_button.append(icon);
   li.append(input);
@@ -28,9 +34,103 @@ function createListItem(title) {
 
   return li;
 }
-
+// OBJECT RELATED
+function createTask(title) {
+  return {
+    id: Date.now().toString(),
+    title: title,
+    completed: false,
+  };
+}
+// ELEMENT RELATED
 function addClasses(element, utlityClassesArray) {
   utlityClassesArray.forEach((utilClass) => {
     element.classList.add(utilClass);
   });
+}
+// ELEMENT RELATED
+function removeListItem(parent, element) {
+  const movedItem = parent.removeChild(element);
+  return movedItem;
+}
+// NOT USED YET -> TO BE RELATED SOMEWHERE
+function showMessage(parent) {
+  const container = document.createElement("div");
+  const message = document.createElement("span");
+  addClasses(message, ["sp-font", "sp-width"]);
+  addClasses(container, [
+    "tasks-list__list-item",
+    "li-bg",
+    "bo-rad-50",
+    "li-width",
+    "li-shad",
+    "delete-animation",
+  ]);
+  message.textContent = "Task moved to the completed list";
+  container.append(message);
+  parent.append(container);
+}
+// ELEMENT RELATED
+function removeTaskAnimation(element) {
+  element.classList.add("delete-animation");
+}
+// OBJECT RELATED
+function populateTasksList(list, task) {
+  list.push(task);
+}
+// STORAGE RELATED
+function populateLocalStorage(key, value) {
+  if (typeof value !== String) {
+    localStorage.setItem(key, JSON.stringify(value));
+  } else {
+    localStorage.setItem(key, value);
+  }
+}
+// STORAGE RELATED
+function parseLocalStorage() {
+  const tasksList = JSON.parse(localStorage.getItem("tasks"));
+  return tasksList;
+}
+// THE BRIDGE TO CONNECT OBJECT WITH ELEMENT -> IN ORDER TO PERFORM SOMETHING
+function matchElementWithObject(task, list) {
+  const task_id = task.getAttribute("data-id");
+  for (let i = 0; i < list.length; i++) {
+    if (list[i].id === task_id) {
+      return {
+        clicked_task: list[i],
+        index: i,
+      };
+    }
+  }
+}
+// OBJECT RELATED
+function modifyTaskState(task, list) {
+  const { clicked_task } = matchElementWithObject(task, list);
+  clicked_task.completed = true;
+  populateLocalStorage("tasks", list);
+}
+// STORAGE RELATED
+function removeTaskFromStorage(task, list) {
+  const { index } = matchElementWithObject(task, list);
+  list.splice(index, 1);
+  populateLocalStorage("tasks", list);
+}
+// STORAGE RELATED
+function displayStoragedItems(list1, list2) {
+  const tasksList = parseLocalStorage();
+  if (!tasksList) return []; // if there is no tasks in storage, return an empty list
+  tasksList.forEach((task) => {
+    if (task.completed === false) {
+      const li = createListItem(task);
+      list1.append(li);
+    } else {
+      const li = createListItem(task);
+      list2.append(li);
+    }
+  });
+  return tasksList;
+}
+// ELEMENT RELATED
+function moveTo(list, list_item) {
+  list.append(list_item);
 }
